bd693c4e4ada2351ce6de9c830b7da203b189cae
nkapotoxin <nkapotoxin@gmail.com>
2016-03-29 16:33:08 +0800

    add pause/unpause get_console_output apis
    Change-Id: I4782947201de07d02068d92a4544bf6e65be4e9a

--- wormholeclient/api/container.py
+++ wormholeclient/api/container.py
@@ -1,12 +1,18 @@
+import requests
+import requests.exceptions
 from .. import utils
+from .. import errors
 
 
 class ContainerApiMixin(object):
 
-    def create_container(self, image_name, volume_id=None, timeout=10):
+    def create_container(self, image_name, volume_id=None, network_info=None, block_device_info=None,
+                         inject_files=None, admin_password=None, timeout=10):
         params = {'t': timeout}
         url = self._url("/container/create")
-        create_config = utils.create_container_config(image_name, volume_id=volume_id)
+        create_config = utils.create_container_config(image_name, volume_id=volume_id, network_info=network_info,
+                                                      block_device_info=block_device_info, inject_files=inject_files,
+                                                      admin_password=admin_password)
         res = self._post_json(url, params=params, data=create_config)
         self._raise_for_status(res)
         return res.raw
@@ -38,5 +44,45 @@ class ContainerApiMixin(object):
         params = {'t': timeout}
         url = self._url("/container/inject-files")
         res = self._post_json(url, data={ 'inject_files' : inject_files })
+        self._raise_for_status(res)
+        return res.raw
+
+    def pause_container(self, timeout=10):
+        params = {'t': timeout}
+        url = self._url("/container/pause")
+        res = self._post_json(url, params=params, data=None)
+        self._raise_for_status(res)
+        return res.raw
+
+    def unpause_container(self, timeout=10):
+        params = {'t': timeout}
+        url = self._url("/container/unpause")
+        res = self._post_json(url, params=params, data=None)
+        self._raise_for_status(res)
+        return res.raw
+
+    def get_console_output(self, timeout=10):
+        params = {'t': timeout}
+        url = self._url("/container/console-output")
+        try:
+            return self._result(self._get(url, params=params), True)
+        except requests.exceptions.ConnectionError as ce:
+            raise errors.ConnectionError()
+        except Exception as e:
+            raise errors.InternalError()
+
+    def set_admin_password(self, admin_password, timeout=10):
+        params = {'t': timeout}
+        url = self._url("/container/admin-password")
+        admin_password_config = utils.admin_password_config(admin_password)
+        res = self._post_json(url, params=params, data=admin_password_config)
+        self._raise_for_status(res)
+        return res.raw
+
+    def create_image(self, image_id, timeout=10):
+        params = {'t': timeout}
+        url = self._url("/container/create-image")
+        create_image_config = utils.create_image_config(image_id)
+        res = self._post_json(url, params=params, data=create_image_config)
         self._raise_for_status(res)
         return res.raw
